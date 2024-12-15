33ece47ae1372a8535f70d405ae57d6c606583cc
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-12-15 21:03:50 +0530

    user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,13 @@
+// user schema/table
+
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const User = sequelize.define("User", {
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, unique: true, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.ENUM("admin", "user"), defaultValue: "user" },
+});
+
+export default User;
