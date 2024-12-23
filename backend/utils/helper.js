c2970408db33eef689ce05a1341ed4f55fa8c534
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-12-23 19:10:09 +0530

    sendEmail function for sending the reset token on email

--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -1,7 +1,33 @@
 //file to generate jwt token
 
 import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
 
 export function generateToken(payload){
     return jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:"1d"});
+}
+
+export async function sendEmail(to,subject,text){
+    try{
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+
+        const mailOptions = {
+          from: `"Nerdy Archive Library" <${process.env.EMAIL_USER}>`,
+          to,
+          subject,
+          text,
+        };
+
+        await transporter.sendMail(mailOptions);
+        console.log(`Email sent to ${to} successfully.`);
+    }catch(error){
+        console.error("Error sending email:", error);
+        throw error;
+    }
 }
