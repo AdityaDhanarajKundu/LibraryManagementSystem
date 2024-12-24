63f1bdd99e76a6013e5f4a68bd837f17c084dc78
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-12-24 21:02:21 +0530

    dashboard route

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,9 @@ import userRoutes from "./routes/userRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import transactionRoutes from "./routes/transactionRoutes.js";
 import fileRoutes from "./routes/fileRoutes.js";
+import { dashBoardStats } from "./controllers/userController.js";
+import { authenticateToken } from "./middleware/authMiddleware.js";
+import { getRecentActivities } from "./controllers/transactionController.js";
 
 dotenv.config();
 
@@ -18,6 +21,8 @@ app.use("/api/users", userRoutes);
 app.use("/api/books", bookRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/files", fileRoutes);
+app.get("/api/dashboard", authenticateToken, dashBoardStats);
+app.get("/api/recent-activities", authenticateToken, getRecentActivities);
 // static files serving
 app.use("/uploads", express.static("uploads/books"));
 app.use("uploads/thumbnails", express.static("uploads/thumbnails"));  
