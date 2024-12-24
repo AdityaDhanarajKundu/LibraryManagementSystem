2985c9daea94a77500deb0df7399e91f4c0bcc41
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-12-25 01:10:29 +0530

    updated the sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -11,9 +11,13 @@ export default function Sidebar() {
         sx={{
           width: "160px", // Increased width to accommodate long words
           backgroundColor: "#3A3A3A",
+          position: "fixed", // Fix the sidebar
+          top: 0,
+          left: 0,
           height: "100vh",
           boxShadow: "2px 0 5px rgba(0, 0, 0, 0.1)",
           padding: "20px",
+          marginTop: "64px",
         }}
       >
         <List>
