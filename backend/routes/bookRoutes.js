585a751581f4e035e99420c509c63f1c63e7db1a
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-12-27 21:45:12 +0530

    changed the order of routes written to avoid mishandling

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,7 +1,7 @@
 // routes the http requests for book related endpoints
 
 import express from "express";
-import { getAllBooks, getBookById, addBook, getBooksByGenre } from "../controllers/bookController.js";
+import { getAllBooks, getBookById, addBook, getBooksByGenre, getBorrowedBooksByUser} from "../controllers/bookController.js";
 import { authenticateToken, isAdmin } from "../middleware/authMiddleware.js";
 import uploadMiddleware from "../middleware/filesMiddleware.js";
 
@@ -9,7 +9,8 @@ const router = express.Router();
 
 router.get("/",authenticateToken, getAllBooks);
 router.get("/categories",authenticateToken, getBooksByGenre);
-router.get("/:id",authenticateToken, getBookById);
+router.get("/borrowed-books", authenticateToken, getBorrowedBooksByUser);
 router.post("/",authenticateToken, isAdmin, uploadMiddleware, addBook);
+router.get("/:id", authenticateToken, getBookById);
 
 export default router;
