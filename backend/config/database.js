32db07dd0e77cd576bd5454ad55062873fed94ab
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-12-31 13:02:19 +0530

    connected to aiven mysql

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,21 +2,38 @@
 
 import Sequelize from "sequelize";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
-    host : process.env.DB_HOST,
-    dialect : "mysql",
-});
+// the aiven ca certificate
+const caCertificate = fs.readFileSync("./certs/ca.pem").toString();
 
-async function connect(){
-    try{
-        await sequelize.authenticate();
-        console.log("Connected to MySQL database");
-    }catch(err){
-        console.log("Unable to connect to MySQL database",err);
-    }
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+        ca: caCertificate, // Ensure only valid certificates are used
+      },
+    },
+  }
+);
+
+async function connect() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connected to Aiven MySQL database");
+  } catch (err) {
+    console.error("Unable to connect to MySQL database", err);
+  }
 }
 
 connect();
