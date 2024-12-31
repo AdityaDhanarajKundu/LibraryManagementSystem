af1b64dc59ca193b1436b4f609f08c4b2a5cf362
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-12-31 15:51:13 +0530

    reinstated the ssl connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,6 +6,9 @@ import fs from "fs";
 
 dotenv.config();
 
+// aiven ca certificate
+const caCertificate = fs.readFileSync("./certs/ca.pem").toString();
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -14,6 +17,13 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+        ca: caCertificate, // Ensure only valid certificates are used
+      },
+    },
   }
 );
 
